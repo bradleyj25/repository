f861e9fcf2e2f0c18393589b2f289e71a7aba9ff
st20103222 <st20103222@s00046.internal.uwic.ac.uk>
2019-02-05 11:56:40 +0000

    img

--- Programming/src/ImageFileHandler.java
+++ Programming/src/ImageFileHandler.java
@@ -1,4 +1,32 @@
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
-public class ImageFileHandler {
+public class ImageFileHandler extends FileHandler {
+	
+	int width; 
+	int height;
+	String image_type; 
+	BufferedImage img; 
+	
+	public ImageFileHandler() {
+		
+	}
+	
+	public ImageFileHandler(String file_name) {
+		super(file_name);
+		img = null;
+	}
+
+	@Override
+	public void readFile() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void writeFile(String file_name) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
